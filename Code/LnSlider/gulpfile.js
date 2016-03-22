340bb2d3ff04678946455363550df8c5926c0a17
Lanz <hanaarena@gmail.com>
2016-03-22 14:57:26 +0800

    formated

--- Code/LnSlider/gulpfile.js
+++ Code/LnSlider/gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
 gulp.task('script', function () {
-	return gulp.src('./src/LnSlider.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('./dist/'))
-	});
+  return gulp.src('./src/LnSlider.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+});
 
 gulp.task('default', ['script']);
--- Code/LnSlider/webpack.config.js
+++ Code/LnSlider/webpack.config.js
@@ -1,17 +1,17 @@
 var webpack = require('webpack');
 
 module.exports = {
-	entry: './src/LnSlider.js',
-	output: {
-		filename: 'bundle.js',
-		path: './dist/'
-	},
-	devtool: 'source-map',
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			}
-		})
-	]
+  entry: './src/LnSlider.js',
+  output: {
+    filename: 'bundle.js',
+    path: './dist/'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 }
